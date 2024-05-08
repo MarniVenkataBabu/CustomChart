c4a424b58eba6b1a48a0fec0c70a7c50c624c9f7
venkat <venkatthekiller@gmail.com>
2024-05-09 00:12:04 +0530

    login page added successfully

--- thirdApp/src/app/layout/home/home.module.ts
+++ thirdApp/src/app/layout/home/home.module.ts
@@ -5,12 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { UsersListModule } from '../users-list/users-list.module';
 import { MessagesModule } from '../messages/messages.module';
 import { HomeComponent } from './home.component';
-
+import { SharedModule } from '../../shared/module/shared.module';
 @NgModule({
   imports: [
     CommonModule,
     HomeRoutingModule,
     FormsModule,
+    SharedModule,
     UsersListModule,
     MessagesModule
   ],
